8411eb41d244bc47ac29885db502d6af89c7949e
ruanhaipeng002 <ruanhaipeng002@ke.com>
2022-11-15 22:04:22 +0800

    mod test_clion

--- test_clion/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-
-using namespace std;
-
-
-int main(int argc, const char* argv[]) {
-    std::cout << "hello world" << std::endl;
-}
---
+++ test_clion/src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <regex>
+
+// #include "json/json.h"
+
+// #include "utilscpp/string_helper.h"
+// #include "utilscpp/file_helper.h"
+// #include "utilscpp/json_helper.h"
+
+
+using namespace std;
+
+
+int main(int argc, const char* argv[]) {
+//    utilscpp::TestStringHelper();
+//    utilscpp::TestFileHelper();
+//    utilscpp::TestJsonHelper();
+
+
+}
